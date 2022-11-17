acabe1f9d0898471cc8b771512604a978c641c33
William Peng <williampenghi@gmail.com>
2022-11-16 21:45:23 -0500

    Skeleton for dress up game, needs docstrings

---
+++ check_in.py
@@ -0,0 +1,92 @@
+"""This program simulates a dress-up game where a user can put clothes on
+themselves."""
+
+class Character:
+    """Allows user to set custom details for their character."""
+    def __init__():
+        self.name
+        self.eye_color
+        
+class Dressup:
+    def __init__(budget, character):
+        pass
+    #initalizes attributes
+    #sets username, budget, etc.
+        self.budget
+        self.character
+        self.closet
+        """compare closets between different characters, prints who has more
+        set operations"""
+    
+    def wear_clothes():
+        pass
+    #wear clothes and removes it from closet
+    
+    def remove_clothes():
+        pass
+    #removes clothes, puts it into closet
+    
+    def closet():
+        pass
+    #stores owned clothes
+    
+    def catalogue():
+        """
+        print csv
+        pass
+        """
+        
+        """lambda function, add a command to return catalogue from cheap to expensive"""
+    #prints catalogue from json file
+    
+    def buy_clothes():
+        """Conditional Expression: if item not in closet and budget ok, buy"""
+        pass
+    #adds clothes to closet, subtracts from budget, checks if you already have
+    
+    def sell_clothes():
+        """Conditional Expression: if item in closet, proceed"""
+        pass
+    #removes clothes from closet, refunds 50%, checks if it's in the closet
+    
+    def __add__():
+        """magic method, returns a list of clothes of that type when prompted"""
+        """pick another name"""
+        """use list comprehension to get the items"""
+        """list = [EXPR for ITERVAR in ITERABLE if CONDITION]"""
+        pass
+    
+    def visualize():
+        """use data vis tools to print out what the character has on"""
+
+        
+def main(catalogue_filepath, savestate=None):
+    """Optional Parameters
+    """
+    """with_statements for opening file"""
+    pass
+    """if savestate != None, read the file and restore progress by filling in 
+    attributes of the character"""
+
+def parse_args(arglist):
+    """argument parser"""
+    pass
+
+if __name__ == "__main__":
+    pass
+    """f-strings"""
+    
+    
+"""Here's the list of the techniques we want to showcase
+
+- conditional expressions - Layla
+- optional parameters and/or use of keyword arguments - William
+- f-strings - Flavyne
+- with statements - William
+- the ArgumentParser class - Mia
+- set operations on sets or frozensets - Mia
+- comprehensions or generator expressions - Layla
+- magic methods other than __init__() - Flavyne
+- concatenating, merging, filtering, or performing groupby operations on Pandas DataFrames - Anna
+- visualizing data with pyplot or seaborn - Anna
+"""
